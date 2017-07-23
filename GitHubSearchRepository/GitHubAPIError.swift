ca70c88f6b7ba0f0cf2daa8c6bcc4fe2c67b1de9
Shizuna Ito <adfa1728@chiba-u.jp>
2017-07-23 12:22:59 +0900

    エラーレスポンスのモデルを追加

---
+++ GitHubSearchRepository/GitHubAPIError.swift
@@ -0,0 +1,60 @@
+//
+//  GitHubAPIError.swift
+//  GitHubSearchRepository
+//
+//  Created by 伊藤静那(Ito Shizuna) on 2017/07/23.
+//  Copyright © 2017年 ShizunaIto. All rights reserved.
+//
+
+import Foundation
+
+struct GitHubAPIError : JSONDecodable, Error {
+    struct FieldError : JSONDecodable {
+        let resource: String
+        let field: String
+        let code: String
+        
+        init(json: Any) throws {
+            guard let dictionary = json as? [String : Any] else {
+                throw JSONDecodeError.invalidFormat(json: json)
+            }
+            
+            guard let resource = dictionary["resource"] as? String else {
+                throw JSONDecodeError.missingValue(key: "resource", actualValue: dictionary["resource"])
+            }
+            
+            guard let field = dictionary["field"] as? String else {
+                throw JSONDecodeError.missingValue(key: "field", actualValue: dictionary["field"])
+            }
+            
+            guard let code = dictionary["code"] as? String else {
+                throw JSONDecodeError.missingValue(key: "code", actualValue: dictionary["code"])
+            }
+            
+            self.resource = resource
+            self.field = field
+            self.code = code
+        }
+    }
+    
+    let message: String
+    let fieldErrors: [FieldError]
+    
+    init(json: Any) throws {
+        guard let dictionary = json as? [String : Any] else {
+            throw JSONDecodeError.invalidFormat(json: json)
+        }
+        
+        guard let message = dictionary["message"] as? String else {
+            throw JSONDecodeError.missingValue(key: "message", actualValue: dictionary["message"])
+        }
+        
+        let fieldErrorObjects = dictionary["errors"] as? [Any] ?? []
+        let fieldErrors = try fieldErrorObjects.map {
+            return try FieldError(json: $0)
+        }
+        
+        self.message = message
+        self.fieldErrors = fieldErrors
+    }
+}
